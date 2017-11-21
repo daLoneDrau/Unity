f3c19b70f328cf01569a9d6e15c4af4b711ce2a9
daLoneDrau <donaldbrau@yahoo.com>
2017-11-21 11:47:12 -0500

    Tue Nov 21 11:47:01 EST 2017

--- git-push-all.sh
+++ git-push-all.sh
@@ -1,4 +1,4 @@
-echo Updating Projects
+echo Committing Project Changes
 
 now=$(date)
 
@@ -7,3 +7,13 @@ git add -A
 git commit -am "$now"
 
 git push origin master
+
+read -n1 -r -p "Press space to continue..." key
+
+if [ "$key" = '' ]; then
+    # Space pressed, do something
+    # echo [$key] is empty when SPACE is pressed # uncomment to trace
+else
+    # Anything else pressed, do whatever else.
+    # echo [$key] not empty
+fi
