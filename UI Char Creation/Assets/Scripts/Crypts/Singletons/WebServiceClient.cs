2ff1ff7c164ab925974e3c6f9929b07aee9a7e2e
daLoneDrau <donaldbrau@yahoo.com>
2018-02-07 10:09:45 -0500

    Wed, Feb 07, 2018 10:09:42 AM

---
+++ UI Char Creation/Assets/Scripts/Crypts/Singletons/WebServiceClient.cs
@@ -0,0 +1,145 @@
+﻿using Assets.Scripts.Crypts.Flyweights;
+using Assets.Scripts.UI.SimpleJSON;
+using RPGBase.Flyweights;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace Assets.Scripts.Crypts.Singletons
+{
+    public class WebServiceClient:MonoBehaviour
+    {
+        private static WebServiceClient instance;
+        /// <summary>
+        /// the singleton instance.
+        /// </summary>
+        public static WebServiceClient Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new WebServiceClient();
+                }
+                return instance;
+            }
+            protected set { }
+        }
+        private WebServiceClient() { }
+        public IEnumerator GetEquipmentElementByCode(string elem, System.Action<int> result)
+        {
+            using (UnityWebRequest www = UnityWebRequest.Get("http://localhost:8080/SW-CTService/sw_ct/equipment_element_types/code/" + elem))
+            {
+                yield return www.Send();
+
+                if (www.isError)
+                {
+                    Debug.Log(www.error);
+                }
+                else
+                {
+                    var str = System.Text.Encoding.Default.GetString(www.downloadHandler.data);
+                    var n = JSON.Parse(str);
+                    if (n.IsArray)
+                    {
+                        int val = 0;
+                        for (int i = 0, li = n.Count; i < li; i++)
+                        {
+                            val = n[i]["value"];
+                        }
+                        result(val);
+                    }
+                }
+            }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="code"></param>
+        /// <param name="result"></param>
+        /// <returns></returns>
+        public IEnumerator GetElementModifierByCode(string code, System.Action<EquipmentItemModifier> result)
+        {
+            using (UnityWebRequest www = UnityWebRequest.Get("http://localhost:8080/SW-CTService/sw_ct/equipment_item_modifiers/code/" + code))
+            {
+                yield return www.Send();
+
+                if (www.isError)
+                {
+                    Debug.Log(www.error);
+                }
+                else
+                {
+                    var str = System.Text.Encoding.Default.GetString(www.downloadHandler.data);
+                    var n = JSON.Parse(str);
+                    if (n.IsArray)
+                    {
+                        EquipmentItemModifier mod = null;
+                        for (int i = 0, li = n.Count; i < li; i++)
+                        {
+                            mod = new EquipmentItemModifier
+                            {
+                                Value = n[i]["value"].AsFloat,
+                                Percent = n[i]["percent"].AsBool
+                            };
+                            break;
+                        }
+                        result(mod);
+                    }
+                }
+            }
+        }
+        public IEnumerator LoadHomelands(Action callback)
+        {
+            Console.WriteLine("++++++++++++++++++++++++++++++++++++++++++++++++++++++LoadHomelands");
+            UnityWebRequest www = new UnityWebRequest("http://localhost:8080/SW-CTService/sw_ct/homelands")
+            {
+                downloadHandler = new DownloadHandlerBuffer()
+            };
+            yield return www.Send();
+            if (www.isError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                var str = System.Text.Encoding.Default.GetString(www.downloadHandler.data);
+                var n = JSON.Parse(str);
+                if (n.IsArray)
+                {
+                    for (int i = 0, li = n.Count; i < li; i++)
+                    {
+                        Homeland homeland = new Homeland(n[i]["name"], n[i]["description"]);
+                        if (n[i]["modifiers"] != null)
+                        {
+                            JSONObject o = n[i]["modifiers"].AsObject;
+                            Console.WriteLine(o.ToString());
+                            foreach (JSONNode node in o.Keys)
+                            {
+                                string elem = node;
+                                string mod = o[elem].ToString();
+                                int e = 0;
+                                Console.WriteLine(elem);
+                                Console.WriteLine(mod);
+                                yield return StartCoroutine(GetEquipmentElementByCode(elem, value => e = value));
+                                EquipmentItemModifier eim = null;
+                                yield return StartCoroutine(GetElementModifierByCode(mod, value => eim = value));
+                                homeland.SetModifier(e, eim);
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("nomodifiers");
+                        }
+                    }
+                }
+                print("finished processing");
+                callback();
+            }
+        }
+    }
+}
---
+++ UI Char Creation/Assets/Scripts/UI/MenuButton.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class MenuButton : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject decoration;
+    private bool currentState = false;
+    public bool CurrentState
+    {
+        get { return currentState; }
+        set
+        {
+            currentState = value;
+            if (currentState)
+            {
+                if (!decoration.activeSelf)
+                {
+                    decoration.SetActive(true);
+                }
+                Text text = gameObject.GetComponentInChildren<Text>();
+                if (text.fontStyle != FontStyle.Bold)
+                {
+                    text.fontStyle = FontStyle.Bold;
+                    text.color = Color.white;
+                }
+            }
+            else
+            {
+                if (decoration.activeSelf)
+                {
+                    decoration.SetActive(false);
+                }
+                Text text = gameObject.GetComponentInChildren<Text>();
+                if (text.fontStyle == FontStyle.Bold)
+                {
+                    text.fontStyle = FontStyle.Normal;
+                }
+            }
+        }
+    }
+    void Awake()
+    {
+        decoration.SetActive(false);
+        EventTrigger.Entry eventtype = new EventTrigger.Entry
+        {
+            eventID = EventTriggerType.PointerEnter
+        };
+        eventtype.callback.AddListener((eventData) => { OnMouseEnter(); });
+
+        gameObject.AddComponent<EventTrigger>();
+        gameObject.GetComponent<EventTrigger>().triggers.Add(eventtype);
+
+        eventtype = new EventTrigger.Entry
+        {
+            eventID = EventTriggerType.PointerExit
+        };
+        eventtype.callback.AddListener((eventData) => { OnMouseExit(); });
+        gameObject.GetComponent<EventTrigger>().triggers.Add(eventtype);
+    }
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnMouseEnter()
+    {
+        if (CurrentState)
+        {
+        }
+        else
+        {
+            Image decoImage = decoration.GetComponent<Image>();
+            if (gameObject.GetComponent<Button>().interactable == true)
+            {
+                decoration.SetActive(true);
+            }
+            else
+            {
+                decoration.SetActive(false);
+            }
+        }
+    }
+    void OnMouseExit()
+    {
+        if (!CurrentState)
+        {
+            decoration.SetActive(false);
+        }
+    }
+}
