522ce1ddf048a47b45cfe7211b9b9b414900c18a
daLoneDrau <donaldbrau@yahoo.com>
2018-02-14 15:22:35 -0500

    Wed Feb 14 15:22:29 EST 2018

--- UI Char Creation/Assets/InventorySlotController.cs
+++ UI Char Creation/Assets/InventorySlotController.cs
@@ -138,27 +138,106 @@ public class InventorySlotController : MonoBehaviour, IDropAccessible
             DragAndDropHandler.DragStart(this);
         }
     }
+    /// <summary>
+    /// flag indicating the cursor has been set after a drag action
+    /// </summary>
     private bool cursorSet = false;
     void OnDrag()
     {
         //print("dragging...............................................");
         // change the cursor to my object
         if (io != null
-            && !cursorSet)
+                && !cursorSet)
         {
             Image img = transform.GetChild(0).GetChild(0).GetComponent<Image>();
             print(img);
             // assume "sprite" is your Sprite object
             var croppedTexture = new Texture2D((int)img.sprite.rect.width, (int)img.sprite.rect.height);
-            var pixels = img.sprite.texture.GetPixels((int)img.sprite.textureRect.x,
-                                                    (int)img.sprite.textureRect.y,
-                                                    (int)img.sprite.textureRect.width,
-                                                    (int)img.sprite.textureRect.height);
+            var pixels = img.sprite.texture.GetPixels((int)img.sprite.textureRect.x, (int)img.sprite.textureRect.y, (int)img.sprite.textureRect.width, (int)img.sprite.textureRect.height);
             croppedTexture.SetPixels(pixels);
+            // draw "arrow" pointing to hotspot
+            DrawLine(croppedTexture,
+                0,
+                (int)img.sprite.textureRect.height - 1,
+                0,
+                (int)img.sprite.textureRect.height - 5,
+                Color.white,
+                2); // vertical line
+            DrawLine(croppedTexture,
+                0,
+                (int)img.sprite.textureRect.height - 1,
+                4,
+                (int)img.sprite.textureRect.height - 1,
+                Color.white,
+                2); // horizontal line
             croppedTexture.Apply();
             Cursor.SetCursor(croppedTexture, Vector2.zero, CursorMode.Auto);
-            //Sprite.Create(texture, rect, pivot);
             cursorSet = true;
+        }
+    }
+    private void DrawLine(Texture2D a_Texture, int x1, int y1, int x2, int y2, Color a_Color, int lineWidth = 1)
+    {
+        float xPix = x1;
+        float yPix = y1;
+
+        float width = x2 - x1;
+        float height = y2 - y1;
+        float length = Mathf.Abs(width);
+        if (Mathf.Abs(height) > length) length = Mathf.Abs(height);
+        int intLength = (int)length;
+        float dx = width / (float)length;
+        float dy = height / (float)length;
+        for (int i = 0; i <= intLength; i++)
+        {
+            if (lineWidth == 1)
+            {
+                a_Texture.SetPixel((int)xPix, (int)yPix, a_Color);
+            }
+            else
+            {
+                if (Mathf.Abs(x1 - x2) > Mathf.Abs(y1 - y2))
+                {
+                    // horizontal line
+                    print("horizontal");
+                    int minY = (int)yPix - (lineWidth / 2);
+                    if (minY + lineWidth - 1 >= a_Texture.height)
+                    {
+                        minY = a_Texture.height - lineWidth;
+                    }
+                    else if (minY < 0)
+                    {
+                        minY = 0;
+                    }
+                    print("minY::" + minY);
+                    for (int j = minY; j < minY + lineWidth; j++)
+                    {
+                        print((int)xPix + "," + j);
+                        a_Texture.SetPixel((int)xPix, j, a_Color);
+                    }
+                }
+                else
+                {
+                    // vertical line
+                    print("vertical");
+                    int minX = (int)xPix - (lineWidth / 2);
+                    if (minX + lineWidth - 1 >= a_Texture.width)
+                    {
+                        minX = a_Texture.width - lineWidth;
+                    }
+                    else if (minX < 0)
+                    {
+                        minX = 0;
+                    }
+                    print("minX::" + minX);
+                    for (int j = minX; j < minX + lineWidth; j++)
+                    {
+                        print(j + "," + (int)yPix);
+                        a_Texture.SetPixel(j, (int)yPix, a_Color);
+                    }
+                }
+            }
+            xPix += dx;
+            yPix += dy;
         }
     }
     void OnEndDrag()
