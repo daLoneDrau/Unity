fd8d9a985b2be3aa422dffb7f4f93c6e957688a4
daLoneDrau <daLoneDrau@users.noreply.github.com>
2018-05-30 00:04:00 -0400

    Update Shortsword.cs

--- Barbarian Prince/Assets/Scripts/Blueholme/Scriptables/Items/Shortsword.cs
+++ Barbarian Prince/Assets/Scripts/Blueholme/Scriptables/Items/Shortsword.cs
@@ -1,4 +1,5 @@
 ﻿using Assets.Scripts.Blueholme.Flyweights;
+using Assets.Scripts.Blueholme.Globals;
 using RPGBase.Constants;
 using System;
 using System.Collections.Generic;
@@ -12,9 +13,11 @@ namespace Assets.Scripts.Blueholme.Scriptables.Items
         public override int OnInit()
         {
             Console.WriteLine("Shortsword ONINIT");
+            UnityEngine.Debug.Log("Shortsword ONINIT");
             BHItemData item = (BHItemData)Io.ItemData;
             item.ItemName = "Short sword";
             item.SetObjectType(EquipmentGlobals.OBJECT_TYPE_1H);
+            item.WeaponSpeed = BHGlobals.LIGHT_WEAPON;
             item.Dice = Dice.ONE_D4;
             item.DmgModifier = 0;
             item.Weight = 7;
