e9ddd3e126d368f4e2231fd6b92a00a9efac10af
daLoneDrau <donaldbrau@yahoo.com>
2018-10-12 15:12:39 -0400

    Fri Oct 12 15:12:26 EDT 2018

---
+++ WoFM RPG/Assets/Scripts/WoFM/UI/2D/TooltipActivator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using WoFM.UI.GlobalControllers;
+
+namespace WoFM.UI._2D
+{
+    public class TooltipActivator : MonoBehaviour
+    {
+        /// <summary>
+        /// The <see cref="Animator"/> that controls the tooltip display.
+        /// </summary>
+        public Animator Animator;
+        public void HideTooltip()
+        {
+            print("Hide tooltip");
+            Animator.Play("Hide Tooltip");
+        }
+        public void ShowTooltip()
+        {
+            if (!GameSceneController.Instance.CONTROLS_FROZEN)
+            {
+                print("Show tooltip");
+                Animator.Play("Show Tooltip");
+            }
+        }
+    }
+}
